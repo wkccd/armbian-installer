8a596000d5d5e9296bc7c7875ab243c01766be84
littlewukong <6682107@gmail.com>
2025-07-22 17:02:55 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+
+mkdir -p imm
+SEND_PATH_FILE="$1"
+filename=$(basename "$SEND_PATH_FILE")  # 提取文件名
+OUTPUT_PATH="imm/$filename"
+mv $SEND_PATH_FILE $OUTPUT_PATH
+
+echo "保存路径: $OUTPUT_PATH"
+
+
+
+echo "✅ 验证文件类型!"
+file "$OUTPUT_PATH"
+
+# 根据扩展名解压
+extension="${filename##*.}"  # 获取文件扩展名
+case $extension in
+  gz)
+    echo "gz正在解压$OUTPUT_PATH"
+    gunzip -f "$OUTPUT_PATH" || true
+    final_name=$(find imm -name '*.img' -print -quit)
+    mv "$final_name" "imm/custom.img"
+    ;;
+  zip)
+    echo "zip正在解压$OUTPUT_PATH"
+    unzip -j -o "$OUTPUT_PATH" -d imm/  # -j 忽略目录结构 
+    final_name=$(find imm -name '*.img' -print -quit)
+    mv "$final_name" "imm/custom.img"
+    ;;
+  xz)
+    echo "xz正在解压$OUTPUT_PATH"
+    xz -d --keep "$OUTPUT_PATH"  # 保留原文件 
+    final_name="${OUTPUT_PATH%.*}"
+    mv "$final_name" "imm/custom.img"
+    ;;
+  *)
+    echo "❌ 不支持的压缩格式: $extension"
+    exit 1
+    ;;
+esac
+
+
+# 检查最终文件
+if [ -f "imm/custom.img" ]; then
+  echo "✅ 解压成功"
+  ls -lh imm/
+  echo "✅ 准备合成 自定义OpenWrt 安装器"
+else
+  echo "❌ 错误：最终文件 imm/custom.img 不存在"
+  exit 1
+fi
+
+mkdir -p output
+docker run --privileged --rm \
+    -v $(pwd)/output:/output \
+    -v $(pwd)/supportFiles:/supportFiles:ro \
+    -v $(pwd)/imm/custom.img:/mnt/custom.img \
+    debian:buster \
+    /supportFiles/custom/build.sh
